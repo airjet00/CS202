970b2bad411cd6255665c6723d86effdab73a142
erikskell <erikskell@gmail.com>
2022-01-21 01:06:24 -0900

    reads to buffer and prints all content

--- HW_0/AP1.cpp
+++ HW_0/AP1.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,6 +8,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+
+    ifstream f("/Users/erikkell/Downloads/Sherlock_Holmes.txt");
+
+    if(f.is_open())
+        cout << f.rdbuf() << endl;
+
+
+    f.close();
     cout << "Hello World" << endl; // for testing
 
     return 0;
