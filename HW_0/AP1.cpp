5233c67eb7e1f0454210d417d0e44bc7b045c7f8
erikskell <erikskell@gmail.com>
2022-01-21 01:42:10 -0900

    Set program to read first line up to 500 words. Using getline

--- HW_0/AP1.cpp
+++ HW_0/AP1.cpp
@@ -8,15 +8,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
+    char words[500];
     ifstream f("/Users/erikkell/Downloads/Sherlock_Holmes.txt");
 
     if(f.is_open())
-        cout << f.rdbuf() << endl;
-
+        f.getline(words, 500);  
+    
+    cout << words << endl;
 
     f.close();
-    cout << "Hello World" << endl; // for testing
+    
+    // cout << "Hello World" << endl; // for testing
 
     return 0;
 }
