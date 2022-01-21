c4bd9ca7d96613254d973927325da7a66b157288
erikskell <erikskell@gmail.com>
2022-01-20 23:20:47 -0900

    Added notes for outline of Temperature convertion code

--- HW_0/main.cpp
+++ HW_0/main.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+
+    //if cmd line variable valid
+    //check ftoc or ctof
+    //call function
 
     cout << "Hello" << endl;
 
     return 0;
 }
+
+// Convert from Fahrenheit to Celsius
+//double cpp_ftoc(const char* str)
+
+// Convert from Celsius to Fahrenheit
+//double c_ctof(const char* str)
