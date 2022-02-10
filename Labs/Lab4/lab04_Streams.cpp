e6022ca4d5c1255d78dec6bb33d5090f06bc340f
erikskell <erikskell@gmail.com>
2022-02-10 09:45:06 -0900

    test file read in lab 4

--- Labs/Lab4/lab04_Streams.cpp
+++ Labs/Lab4/lab04_Streams.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]){
 
     std::string ln;
-    std::ifstream file ("test.txt");
+    std::ifstream file ("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/lab04_Streams.cpp");
     if(file.is_open()){
         while(getline(file, ln)){
             std::cout << ln << std::endl;
