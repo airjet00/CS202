46723e3f20866ebee0150b6ccbafea4740f1d625
erikskell <erikskell@gmail.com>
2022-02-10 10:35:34 -0900

    added input/output for line num and print count

--- Labs/Lab4/lab04_Streams.cpp
+++ Labs/Lab4/lab04_Streams.cpp
@@ -8,9 +8,20 @@ int main(int argc, char *argv[]){
     std::string ln;
     std::ifstream file ("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/lab04_Streams.cpp");
     if(file.is_open()){
+        int count = 0;
         while(getline(file, ln)){
+            std::cout << count+1 << ":  ";
             std::cout << ln << std::endl;
+            count++;
         }
+        std::cout << "Please enter number of copies to print\n" <<
+                        "then space and number line to print: ";
+        
+        int numPrint, numLine;
+        std::cin >> numPrint >> numLine;
+
+        std::cout << "Entered: " << numPrint << " and " << numLine << std::endl;
+
         file.close();
     }else{
         std::cout << " Not working/opening" << std::endl;
