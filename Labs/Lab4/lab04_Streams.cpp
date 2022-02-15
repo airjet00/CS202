e82469dfa011230497b7c45247623e1ce928e221
erikskell <erikskell@gmail.com>
2022-02-14 19:02:19 -0900

    Comment out prior code and using class pp/code examples. Display with number line working

--- Labs/Lab4/lab04_Streams.cpp
+++ Labs/Lab4/lab04_Streams.cpp
@@ -30,58 +30,105 @@ standard output.
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-#include <string>
 
-//TODO: add function for opening file
-//TODO: add function for displaying file
-//TODO: add function for desplaying selected info  
+using std::ofstream;
+using std::ifstream;
+using std::fstream;
+using std::string;
+using std::cout;
+using std::endl;
+
+string fileName = "/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/test.txt";
+
 
 int main(int argc, char *argv[]){
 
-    //display file with number lines
-    std::string ln;
-    std::ifstream file; 
-    file.open("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/test.txt");
-
-    int count = 0;
-    if(file.is_open()){
-        while(getline(file, ln)){
-            std::cout << count+1 << ":  ";  //creates number line
-            std::cout << ln << std::endl;
-            count++;
-        }
-        file.close();
-    }else{
-        std::cout << " Not working/opening" << std::endl;
+    // display file with number lines
+    string ln;    
+    ifstream inFile(fileName);
+    if(inFile.is_open()){
+        cout << fileName << " is open.\n";
     }
 
-    //get number of copies and number line
-    std::cout << "Please enter number of copies to print\n" <<
-                    "then space and number line to print: ";
-    int numPrint, numLine;
-    std::cin >> numPrint >> numLine;
-    std::cout << "Entered: " << numPrint << " and " << numLine << std::endl;
-    //find line and print n number of times 
-    count = 0;
+    int count=0;
+    while (true)
+    {
+        string ln;
+        count++;
+        cout << count << ".  ";
+        getline(inFile, ln);
+        if(!inFile){                        //TODO figure out this line
+            if(inFile.eof()){
+                cout << "Completed reading file.\n";
+                            
+            }else{
+                cout << "Error.\n";
+        }
+        break;
+        }
+        cout << ln << "\n";
+        
+
+    }
     
-    //TODO: Different way to get to begining of file again?
-    file.open("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/test.txt");
 
-    if(file.is_open()){
-        while(getline(file, ln)){
-            count++;
-            //When count equals requested display line
-            if(count == numLine){
-                for(int i =0; i < numPrint; i++){
-                    std::cout << ln << std::endl;
-                }
-            }
-        }
 
-        file.close();
-    }else{
-        std::cout << " Not working/opening" << std::endl;
-    }
+
+// #include <cstdlib>
+// #include <fstream>
+// #include <iostream>
+// #include <string>
+
+// //TODO: add function for opening file
+// //TODO: add function for displaying file
+// //TODO: add function for desplaying selected info  
+
+// int main(int argc, char *argv[]){
+
+//     //display file with number lines
+//     std::string ln;
+//     std::ifstream file; 
+//     file.open("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/test.txt");
+
+//     int count = 0;
+//     if(file.is_open()){
+//         while(getline(file, ln)){
+//             std::cout << count+1 << ":  ";  //creates number line
+//             std::cout << ln << std::endl;
+//             count++;
+//         }
+//         file.close();
+//     }else{
+//         std::cout << " Not working/opening" << std::endl;
+//     }
+
+//     //get number of copies and number line
+//     std::cout << "Please enter number of copies to print\n" <<
+//                     "then space and number line to print: ";
+//     int numPrint, numLine;
+//     std::cin >> numPrint >> numLine;
+//     std::cout << "Entered: " << numPrint << " and " << numLine << std::endl;
+//     //find line and print n number of times 
+//     count = 0;
+    
+//     //TODO: Different way to get to begining of file again?
+//     file.open("/Users/erikkell/Programming/CS202/CS202/Labs/Lab4/test.txt");
+
+//     if(file.is_open()){
+//         while(getline(file, ln)){
+//             count++;
+//             //When count equals requested display line
+//             if(count == numLine){
+//                 for(int i =0; i < numPrint; i++){
+//                     std::cout << ln << std::endl;
+//                 }
+//             }
+//         }
+
+//         file.close();
+//     }else{
+//         std::cout << " Not working/opening" << std::endl;
+//     }
 
     return 0;
 }
