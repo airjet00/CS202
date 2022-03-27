1d456fc251fdfd1424dc1e9d69de630e04bbee25
erikskell <erikskell@gmail.com>
2022-03-26 23:37:16 -0800

    created midterm file and main. created started code for reading and printing from text file

---
+++ Midterm Exam/main.cpp
@@ -0,0 +1,72 @@
+/************************************************
+    Author: Erik Kell         Date: 26 March 2022
+    Class: CS 202             Title: Midterm Exam
+
+****Purpose****
+Midterm exam. Code nneds to be clear, concise,
+and readable. Create a pdf report containing
+source code and first 10 pages of output for a
+book for EACH problem.
+
+****What to Do****
+
+    1. (Version of HW3: Additional Program 1)
+        Write a program that reads in a book
+        (Project Gutenberg or similar) and
+        outputs the text based on given characters
+        wrapping value.
+
+        Usage: prettyprint 768.txt 50
+        Should print the book with a max of 50 char
+        per line.
+
+    2.  Modify program from #1 to print the text in
+        two column format. The command-line arguments
+        are the input file name, the number of lines
+        per page, the width of the columns, and the
+        number of blank spaces between the columns.
+
+        Usage: twocolumn 768.txt 40 38 4
+        Should print 40 lines per page, with each
+        column a max of 38 char wide and 4 blank
+        spaces between the columns. Output
+        "-----------" to show each page break.
+
+****Optional next steps****
+N/A
+
+************************************************/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+//using namespace std;
+using TYPE = std::string;
+int main(int argc, char *argv[]){
+
+    std::string fileName = "/Users/erikkell/Programming/CS202/CS202/testfile.txt";
+
+    std::vector<std::vector<TYPE> > text;
+
+    std::ifstream fin(fileName);
+    for(std::string line; getline(fin, line);)
+    {
+    std::stringstream ss(line);
+        std::vector<TYPE> row;
+        for(TYPE d; ss >> d; ) row.push_back(d);
+        text.push_back(row);
+    }
+
+    std::cout << "Your text:\n";
+    for(auto &row : text)
+    {
+        for(auto &item : row) std::cout << std::setw(5) <<item << ' ' ;
+        std::cout << '\n';
+    }
+
+    return 0;
+}
